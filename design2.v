90542a213a76a7259b4cc2b7f297a5bd6bc57675
bhanuy <89624337+bhan0234@users.noreply.github.com>
2024-03-05 23:04:43 +0530

    Create design2.v

---
+++ design2.v
@@ -0,0 +1,10 @@
+`timescale 1ps / 1ps
+
+module APPCOMP4to2 (X1, X2, X3, X4, Sum, Carry);
+
+ input X1, X2, X3, X4; 
+ output Carry, Sum; 
+ assign Sum =(X2 & ~X3 & ~X4) | (X2 & X3 & X4) | (X1 & ~X3 & ~X4) | (X1 & X3 & X4) | (X1 & X2) ;
+ assign Carry = (X3 | X4) ;
+
+endmodule
