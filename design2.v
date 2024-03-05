ada0e3f167757ad79ee16e7e97196b7271b202d8
bhanuy <89624337+bhan0234@users.noreply.github.com>
2024-03-05 23:50:19 +0530

    Update design2.v

--- design2.v
+++ design2.v
@@ -4,7 +4,7 @@ module APPCOMP4to2 (X1, X2, X3, X4, Sum, Carry);
 
  input X1, X2, X3, X4; 
  output Carry, Sum; 
- assign Sum =(X2 & ~X3 & ~X4) | (X2 & X3 & X4) | (X1 & ~X3 & ~X4) | (X1 & X3 & X4) | (X1 & X2) ;
+ assign Sum =(~(X3^X4) & (X1 | X2)) | (X1 & X2);
  assign Carry = (X3 | X4) ;
 
 endmodule
